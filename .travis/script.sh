4d5b5105bc47f4c3904574ba8023461ced51de77
FiveDimensions <mjing@google.com>
2020-03-11 12:03:44 +0800

    try cc1352 and cc2538 with cmake:wq

--- .travis/script.sh
+++ .travis/script.sh
@@ -108,18 +108,30 @@ python --version || die
 }
 
 build_cc1352() {
-     git checkout -- . || die
-     git clean -xfd || die
-     ./bootstrap || die
-     COMMISSIONER=1 JOINER=1 SLAAC=1 DHCP6_CLIENT=1 DHCP6_SERVER=1 DNS_CLIENT=1 make -f examples/Makefile-cc1352 || die
-     arm-none-eabi-size  output/cc1352/bin/ot-cli-ftd || die
-     arm-none-eabi-size  output/cc1352/bin/ot-cli-mtd || die
-     arm-none-eabi-size  output/cc1352/bin/ot-ncp-ftd || die
-     arm-none-eabi-size  output/cc1352/bin/ot-ncp-mtd || die
+    # git checkout -- . || die
+    # git clean -xfd || die
+    # ./bootstrap || die
+    # COMMISSIONER=1 JOINER=1 SLAAC=1 DHCP6_CLIENT=1 DHCP6_SERVER=1 DNS_CLIENT=1 make -f examples/Makefile-cc1352 || die
+    # arm-none-eabi-size  output/cc1352/bin/ot-cli-ftd || die
+    # arm-none-eabi-size  output/cc1352/bin/ot-cli-mtd || die
+    # arm-none-eabi-size  output/cc1352/bin/ot-ncp-ftd || die
+    # arm-none-eabi-size  output/cc1352/bin/ot-ncp-mtd || die
+    export CC1352FLAGS="-DOPENTHREAD_CONFIG_COMMISSIONER_ENABLE=1         \
+        -DOPENTHREAD_CONFIG_JOINER_ENABLE=1               \
+        -DOPENTHREAD_CONFIG_DHCP6_CLIENT_ENABLE=1         \
+        -DOPENTHREAD_CONFIG_DHCP6_SERVER_ENABLE=1         \
+        -DOPENTHREAD_CONFIG_IP6_SLAAC_ENABLE=1                    \
+        -DOPENTHREAD_CONFIG_DNS_CLIENT_ENABLE=1"
+    git checkout -- . || die
+    git clean -xfd || die
+    mkdir build && cd build || die
+    cmake -GNinja -DOT_PLATFORM=cc1352 -DOT_COMPILE_WARNING_AS_ERROR=ON ${CC1352FLAGS} .. || die
+    ninja || die
+    cd .. || die
 }
 
 build_cc2538() {
-    export CPPFLAGS="-DOPENTHREAD_CONFIG_COMMISSIONER_ENABLE=1         \
+    export CC2538FLAGS="-DOPENTHREAD_CONFIG_COMMISSIONER_ENABLE=1         \
         -DOPENTHREAD_CONFIG_JOINER_ENABLE=1               \
         -DOPENTHREAD_CONFIG_DHCP6_CLIENT_ENABLE=1         \
         -DOPENTHREAD_CONFIG_DHCP6_SERVER_ENABLE=1         \
@@ -134,9 +146,8 @@ build_cc2538() {
 
     git checkout -- . || die
     git clean -xfd || die
-    ./bootstrap || die
     mkdir build && cd build || die
-    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=examples/platforms/cc2538/arm-none-eabi.cmake -DOT_PLATFORM=cc2538 -DOT_COMPILE_WARNING_AS_ERROR=ON ${CPPFLAGS} .. || die
+    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=examples/platforms/cc2538/arm-none-eabi.cmake -DOT_PLATFORM=cc2538 -DOT_COMPILE_WARNING_AS_ERROR=ON ${CC2538FLAGS} .. || die
     ninja || die
     cd .. || die
     #arm-none-eabi-size  examples/apps/cli/ot-cli-ftd || die
